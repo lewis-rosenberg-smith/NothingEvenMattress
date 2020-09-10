e21f99e3ade0571a864d0854073fb9f68bf41518
philc <32604473+Phil160@users.noreply.github.com>
2020-09-11 11:01:47 +1200

    made first reducer but get summary not returning expected data

--- client/components/PicList.jsx
+++ client/components/PicList.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Picture from './Picture'
 import Information from './Information'
+import { getSummary } from '../api/index.js'
+import { recievePictures } from '../actions'
 
 class PicList extends React.Component {
+  componentDidMount() {
+    getSummary().then(data => {
+      this.props.dispatch(recievePictures(data))
+    })
+  }
+
   render() {
     return (
       <>
         <h2>PicList</h2>
-        <Picture />
+        {this.props.pictures.map(pic => {
+          // <Picture />
+          return <p>{Object.keys(pic)}</p>
+        })}
+
         <Information />
       </>
     )
   }
 }
 
-export default PicList
+const mapStateToProps = (state) => {
+  return {
+    pictures: state.pictures
+  }
+}
+
+export default connect(mapStateToProps)(PicList)
