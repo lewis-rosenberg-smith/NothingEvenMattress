1a412b8c7f642c445018ebbe761b1e023531702a
Lewis Rosenberg Smith <lrosenbergsmith@gmail.com>
2020-09-10 23:46:04 +1200

    patch route done

--- server/routes/api.js
+++ server/routes/api.js
@@ -34,7 +34,21 @@ router.post("/", (req, res) => {
 });
 
 router.patch('/:id', (req,res) => {
-  return res.json({message:db.updatePosition(1, 'update')})
+  const id = req.params.id
+  const position = {
+    img_url: req.body.img_url || "",
+    img_name: req.body.img_name || "",
+    position_name: req.body.position_name || "",
+    position_description: req.body.position_description || "",
+    temperature: req.body.temperature || "",
+    participants: req.body.participants || 0,
+    rating: req.body.rating || 0,
+    animal: req.body.animal || "",
+  }
+  return db.updatePosition(id, position)
+  .then((count) => { 
+  return res.json({body:count})
+  })
 })
 
 router.delete('/:id', (req, res) => {
