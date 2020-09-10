aa3ad6bef48f9edd1f26f3b99c8707af23db548c
Anthony McGrath <akm@anthonykyle.co.nz>
2020-09-11 10:01:56 +1200

    Created potential summary function

--- server/db/index.js
+++ server/db/index.js
@@ -2,8 +2,13 @@ const knex = require('../../knexfile')
 const config = process.env.NODE_ENV || 'development'
 const database = require('knex')(knex[config])
 
-function getSummary(){
-  return 'summary'
+function getSummary(db = database){
+  return db('positions')
+    .select('id')
+    .select('position_name')
+    .select('img_url')
+    .select('img_description')
+    .then(res => res)
 }
 
 function getPositionDetail(id){
