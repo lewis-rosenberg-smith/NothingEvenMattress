c2765be1837b44176a57a5b933e22c49f9eed08f
Lewis Rosenberg Smith <lrosenbergsmith@gmail.com>
2020-09-10 23:16:10 +1200

    update position api route complete

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,31 +1,40 @@
-const express = require('express')
-const db = require('../db')
+const express = require("express");
+const db = require("../db");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req,res) => {
-  return db.getSummary()
-    .then(summary => {
-      return res.json(summary)
-    })
-  
-})
+router.get("/", (req, res) => {
+  return db.getSummary().then((summary) => {
+    return res.json(summary);
+  });
+});
 
-router.get('/:id', (req,res) => {
-  return res.json({message:db.getPositionDetail(1)})
-})
+router.get("/:id", (req, res) => {
+  return res.json({ message: db.getPositionDetail(1) });
+});
 
-router.post('/', (req,res) => {
-  return res.json({message:db.addPosition('position')})
-})
+router.post("/", (req, res) => {
+  const position = {
+    img_url: req.body.img_url,
+    img_name: req.body.img_name,
+    position_name: req.body.position_name,
+    position_description: req.body.position_description,
+    temperature: req.body.temperature,
+    participants: req.body.participants,
+    rating: req.body.rating,
+    animal: req.body.animal,
+  };
+  return db.addPosition(position).then((ids) => {
+    return res.json({body: ids[0]});
+  });
+});
 
-router.patch('/:id', (req,res) => {
-  return res.json({message:db.updatePosition(1, 'update')})
-})
+router.patch("/:id", (req, res) => {
+  return res.json({ message: db.updatePosition(1, "update") });
+});
 
-router.delete('/:id', (req, res) => {
-  return res.json({message:db.deletePosition(1)})
-})
+router.delete("/:id", (req, res) => {
+  return res.json({ message: db.deletePosition(1) });
+});
 
-
-module.exports = router
+module.exports = router;
