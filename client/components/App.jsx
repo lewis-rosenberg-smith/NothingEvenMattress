e3f50d67a7dc94809641021dcf1feaa23aa9c59e
Connor-ONeale <connoroneale@gmail.com>
2020-09-11 14:55:38 +1200

    edit seeds pt2

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Delete from './Delete'
+
 
 import PicList from './PicList'
 
@@ -6,6 +8,7 @@ const App = () => {
   return (
     <>
       <h1>App</h1>
+      <Delete id={2} />
       <PicList />
     </>
   )
